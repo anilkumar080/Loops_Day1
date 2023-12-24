12ab52ec4fdaa2e729a29947c36c2089d43a99dd
anilkumar080 <154390074+anilkumar080@users.noreply.github.com>
2023-12-24 14:58:24 +0530

    Add files via upload

---
+++ NumberTraingle.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class NumberTraingle
+{
+    public static void main(String[] args)
+    {
+        int n=4;
+        int count=0;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                count=count+1;
+                System.out.print(" ");
+                System.out.print(count);
+            }
+            System.out.println("");
+
+        }
+
+
+    }
+}
